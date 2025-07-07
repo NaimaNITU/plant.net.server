9cee7e653705b0374b72827bd79041b51448acd0
Robiul Hasan Raian <toktikproject@gmail.com>
2025-07-07 12:46:31 +0600

    fetch single plant data based on id

--- index.js
+++ index.js
@@ -90,6 +90,14 @@ async function run() {
       res.send(result);
     });
 
+    //get a single data based on id from db
+    app.get("/plants/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await plantsCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
